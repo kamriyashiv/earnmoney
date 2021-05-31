8597db4a6a80f23297d452b28e5475f01e947000
kamriyashiv <78133211+kamriyashiv@users.noreply.github.com>
2021-05-31 11:16:04 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -66,86 +66,128 @@ claim.addEventListener('click',function(){
 })
 
 
-
+//FUNCTION FOR MY POINTS VALUE AUTOMATE FIELD  IN MY WITHDRAWAL FROM INPUT FIELD 
 function mypointsAccess(){
+   //ACCESS MY FORM INPUT FIELD AND SET MY POINTS INNTER TEXT IN  THIS INPUT FIELD
    acpoints.value=points.innerHTML;
-   console.log(acpoints.value);
+   //console.log(acpoints.value);
 }
-   function cmplteAd(){
-	    setTimeout(function(){
-		 
-		  var celebration = document.getElementsByClassName('celebration');
-		   var h = document.getElementsByClassName('mainad');
-			for(var i =0;i<h.length;i++){
-				if(h[i].style.display==="block"){
-				h[i].style.display = "none";
-				document.querySelector('.celebration').style.display = "block";
-				mycelebration();
-			}else{
-				
-			}
+
+//FUNCTION FOR MY AD IS COMPLETE
+function cmplteAd(){
+	//CREATE SET TIMEOUT FUNCTION
+	setTimeout(function(){
+	 //CRAETE VAR AND ACCESS CELEBRATION CLASS 
+	  var celebration = document.getElementsByClassName('celebration');
+	  //CRAEET VAR AND ACCESS MAIN AD CLASS
+	   var h = document.getElementsByClassName('mainad');
+	   //SET LOOP FOR H VARAIBLE OR MAIN AD CLASSESS
+		for(var i =0;i<h.length;i++){
+			//SET IF STATEMENT FOR ACTIONS
+			if(h[i].style.display==="block"){
+			h[i].style.display = "none";
+			//CELEBRATION IS DISPLAY NONE AFTER CLICK IN AD
+			
+			document.querySelector('.celebration').style.display = "block";
+			
+			
+			mycelebration();
+		}else{
+			
 		}
-	},45000);
-   }
+	}
+	
+    },45000);//SET DURATION FOR MY AD IS AUTOMATIC CLOSE AFTER 45 SECOND
+
+}//CALL FUNCTION IN MY CLICK AD 
   
-  
+  //FUNCTION FOR CELEBRATION TIME OUT 
   function mycelebration(){
+	  //SET TIMEOUT FUNCTION FOR CLOSE AUTOMATIC MY CELEBRATION IS HIDE
 	   setTimeout(function(){
+		   //ACCESS CELEBRATION CLASS AND SET Style NONE 
 	       document.querySelector('.celebration').style.display = "none";
-       },6000);
+		   
+       },4000);//SET DURATION AFTER MY CELEBRATION IS HIDE
   }
+  //CALL THIS FUNCTION IN MYCOMPLETAD FUNCTION 
    
 
 
+//CODE FOR MY TASK BLOCK IS AUTOMATIC HIDE AFTER CLICK 
+//ACCESS AD CLASS FROM TASK SECTION
 var closeAd = document.querySelectorAll('.ad');
+//ACCESS ALL AD CLASS
 for(var i=0;i<closeAd.length;i++){
+	//SET CLICK EVENT IN ALL AD CLASSES 
 	closeAd[i].addEventListener('click',function(){
+		//SET TIMOUT FUNCTION FOR DISPLAY NONE MY AD BLOCK 
 		setTimeout(()=>{
 			this.style.display = "none";
-		},50000);
+		},3000);//AFTER 3 SECOND DURATION MY AD BLOCK IS AUTOMATIC HIDE
 			
 	})
 }
 
 
-
+//FUNCTION FOR CLICK MY AD AND OPEN HIS AD 
 function myclick(evt,myad){
 	//alert("hello");
+	//ACCESS MAIN AD CLASS
 	var ad = document.getElementsByClassName('mainad');
+	//ACCESS ALL MAIN AD CLASS 
 	for(var i =0;i<ad.length;i++){
+		//SET DEFAULT DISPLAY NONE
 		ad[i].style.display = "none";
 	}
+	//WHEN USER CLICK IN TASK BLOCK HIS AD IS DISPLAY BLOCK 
 	document.getElementById(myad).style.display = "block";
+	
+	
 	cmplteAd();
 }
 
+
+
+//CODE FOR MY GUID TOOL 
+//ACCESS MY CLOSE ID IN INDEX.HTML AND SET CLICK EVENT FOR HIDE GUID TOOL
 myclose.addEventListener('click',function(){
+	//SET Style FOR HIDE
 	document.querySelector('.guidtool').style.display = "none";
 })
+
+
+//FUNCTION FOR ABOUT BASE DETAILS  
 function mybase(evt,bae){
-	//alert("hello");
+	
+	//CREATE VAR AND ACCESS BASE CLASSES IN ABOUT SECTION
 	var ba = document.getElementsByClassName('base');
 	for(var i =0;i<ba.length;i++){
+		//SET DEFAULT STYLE NONE IN ALL BASE CLASSESS
 		ba[i].style.display = "none";
 	}
+	//WHEN MY CLICK IN BASE AFTRE HIS DETAILS IS BLOCK 
 	document.getElementById(bae).style.display = "block";
 	
-}
+}//CALL THIS FUNCTION IN ABOUT SECTION ANCHORE (MANUALY)
+
+
 function prohold(){
 	(profilePage.style.display ==="none")?profilePage.style.display = "block" : profilePage.style.display = "none"
-	
 }
 
 
-
+//FUNCTION FOR NAVIGATION TABS
 function mypage(evt,cp){
+	//ACCESS ALL PAGE CLASSES FROM  INDEX PAGE 
 	 var page = document.getElementsByClassName('page');
 	 for(var i = 0;i < page.length;i++){
+		 //SET DEFAULT Style NONE IN ALL PAGE CLASS
 		  page[i].style.display="none";
 	 }
 	 document.getElementById(cp).style.display="block";
 }
-
+//CALL THIS FUNCTION IN ALL SECTION ANCHORE IN INDEX  (MANUALY)
 
 
 
